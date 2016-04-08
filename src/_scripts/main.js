d220f261cfa123c7a2c5c54f2fc699e47623eaa6
yeion7 <jahir045@gmail.com>
2016-04-08 12:50:09 -0500

    tmp

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -10,10 +10,8 @@ require('../_modules/organisms/header/header');
 require('../_modules/atoms/sala/sala');
 require('../_modules/molecules/card/card');
 
-
-$('body').resize(function(){
-
-  if ($('.header').width() > 1200 ){
+$('body').resize(function () {
+  if ($('.header').width() > 1200) {
     $('.header-menu-nav-items').css('display', 'flex');
   }
 
